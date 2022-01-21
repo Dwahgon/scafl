15053ea54112de8ada59ed6279026360232b2601
DragonOfWar <therealdragonofwar@gmail.com>
2022-01-20 21:06:23 -0300

    Fix project module paths

--- src/scafl/__main__.py
+++ src/scafl/__main__.py
@@ -1,3 +1,8 @@
+import sys
+from os.path import dirname, abspath
+
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+
 import scafl.main
 
 scafl.main.main()
--- src/scafl/main.py
+++ src/scafl/main.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 import sys
+from os.path import dirname, abspath
+
+module_dir = dirname(dirname(abspath(__file__)))
+if sys.path[0] != module_dir:
+    sys.path.insert(0, module_dir)
 
 from scafl.application import Application
 
